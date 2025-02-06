52cdf5b867c966efaf6b39258e617c3aebaff33d
CodingOrangutang0 <bernhard.oma11@gmail.com>
2025-02-06 13:36:03 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import time 
+import BeautifulSoup
+import requests # This is to get a hold of the pictures and the .stl files
+import pyautogui# this is to be able to automatically start the slicer. This version is dedicated to the ellegoo cura slicer.
+# this is an 3d printer assistant that should run in the background and automatically print a .stl file for you through octoprint
+def downloadfile():
+    pass
+def downloadfilewithpicture()
+    pass
+def downlaoadfilewithpictureandslice().
+    pass
+def downloadfilewithvoice()
+    pass
+def downloadfileandpicturewithvoice
+    pass
+def downloadfileandpictureaandslicewithvoice():
+    pass
+  
